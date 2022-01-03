4531424a9d7f836fd7542377bb8c145221154df6
Muhammad Ali <86810654+rizviialii@users.noreply.github.com>
2022-01-03 09:48:48 -0500

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,16 +1,16 @@
 import random
-
 def passwordGenerator(length):
     #type of charaters present in password
-    capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    smallLetters = "abcdefghijklmnopqrstuvwxyz"
+    letters = "abcdefghijklmnopqrstuvwxyz" 
     numbers = "0123456789"
     symbols = "!@#$%^&*()"
     #creating password
-    all = capitalLetters + smallLetters + numbers + symbols
+    all = letters.upper() + letters + numbers + symbols
     password = "".join(random.sample(all,length))
     #return password
     return password
 
-print(passwordGenerator(16))
+#driver code
+print(passwordGenerator(4))
+
 
